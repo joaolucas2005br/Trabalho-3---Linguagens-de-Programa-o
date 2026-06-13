346aba5eb5b2218edb734bff0f09531a5c884494
Henrique Garcia <henriquegarcia1597@gmail.com>
2026-06-12 21:49:14 -0300

    lots && lots && lots && lots of refactor

---
+++ src/GameLogic/Game.hs
@@ -0,0 +1,57 @@
+module GameLogic.Game (
+    gameLoop,
+    startGame,
+) where
+
+import Utils.Play
+import Bots.Bots
+import Utils.InOut
+
+gameLoop :: IO ()
+gameLoop = do
+    
+    clearScreen
+
+    printMenu
+
+    mbBotChoice <- readInt
+
+    handleMaybeInt mbBotChoice gameLoop(\botChoice ->
+            case botChoice of
+                0 -> return ()
+                1 -> startGame botChoice
+                2 -> startGame botChoice
+                3 -> startGame botChoice
+                _ -> do
+                    invalidInput
+                    gameLoop
+            )
+                    
+startGame :: Int -> IO ()
+startGame botChoice = do
+    clearScreen
+
+    printMoves
+
+    mbPlayChoice <- readInt
+
+    handleMaybeInt mbPlayChoice gameLoop (\playChoice ->
+            if not (validPlay playChoice)
+                then do
+                    invalidInput
+                    gameLoop
+                else do
+                    let playerPlay = numberToPlay playChoice
+
+                    botPlay <- chooseBot botChoice playerPlay
+
+                    clearScreen
+
+                    printPlays playerPlay botPlay
+
+                    printResult playerPlay botPlay
+
+                    waitEnter
+
+                    gameLoop
+        )
